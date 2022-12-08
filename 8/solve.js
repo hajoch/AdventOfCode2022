fb6116e7f70f07b5eeec95acd78caa6e1caeb6d8
Hallvard <hallvard.christensen@remarkable.com>
2022-12-08 19:49:03 +0100

    Day 8 done

--- 8/solve.js
+++ 8/solve.js
@@ -1,23 +1,19 @@
 const fs = require('fs');
 
-// Methods
-
 fs.readFile('input.txt', function(err, data) {
     if(err) throw err;
     const rows = data.toString().split("\n").map((row) => row.split("").map(tree => parseInt(tree)));
-
+    // Methods
     const getViews = (x, y) => {
         let row = [...rows[y]];
         let column = rows.map(row => row[x]);
         return [...getRowView(row, x), ...getRowView(column,y)];
     }
-
     const getRowView = (row, index)  => {
         let left = row.splice(0,index).reverse();
         row.shift();
         return [left, row];
     }
-
     const isVisible = (tree, x,y) => {
         let views = getViews(x,y);
         return views.some(view => {
@@ -41,30 +37,13 @@ fs.readFile('input.txt', function(err, data) {
         row.forEach((tree, x) => {
             let scores = getViews(x,y).map((view) => {
                 let score = view.findIndex(area => area >= tree);
-                return score == -1 ? view.length : score;
+                return score == -1 ? view.length : score+1;
             });
             let scenicScore = scores.reduce((acc, score) => acc*score, 1);
             if(scenicScore > highestScenicScore) {
                 highestScenicScore = scenicScore;
-                console.log(scenicScore, scores);
             }
         });
     });
     console.log('Part 2', highestScenicScore);
 });
-
-
-/*
-
-
-    const dimensions = {
-        x: rows[0].length,
-        y: rows.length
-    };
-
-    const isEdge = (x,y) => {
-        if(x === 0 || x === dimensions.x) return true;
-        if(y === 0 || y === dimensions.y) return true;
-        return false;
-    }
-*/
